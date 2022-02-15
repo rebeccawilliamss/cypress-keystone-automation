c0b5bfdadf293c618f996735324025f1fa98303f
r2williams <rebecca.williams@historicengland.org.uk>
2022-02-15 13:07:08 +0000

    Added POM, login tests, home page and login page and custom command for login

---
+++ cypress/POM/login_page.js
@@ -0,0 +1,12 @@
+class LoginPage {
+
+}
+
+const webElements = {
+    username: '//input[@name="username"]',
+    password: '//input[@name="password"]',
+    signInBtn: '//button[@type="submit"]'
+
+};
+
+export {webElements};
