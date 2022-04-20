32f14c4314dbedce817d51b11e856e7cfef047c9
Rebecca <47248246+rebeccawilliamss@users.noreply.github.com>
2022-04-20 21:14:01 +0100

    Update auth.spec.js

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -19,14 +19,14 @@ describe('Login functionality', () => {
 
     it('displays an error message when logging in with invalid credentials', () => {
 
-        cy.login({ username: 'admin', password: 'admin'});
+        cy.login({ username: '/', password: '/'});
 
         cy.get('#login-failed-alert').should('be.visible').and('contain', 'Login failed');
     })
 
     it('can login using valid credentials', () => {
 
-        cy.login({ username: 'RWilliams429', password: '!Ticket39'});
+        cy.login({ username: '/', password: '/'});
     })
 
 });
